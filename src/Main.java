c1688f1878d3ccb8202630e81f80fcb4ca9d4800
Arikpele <79112835522@y.ru>
2022-11-30 11:47:32 +0300

    задание3

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,17 @@ public class Main {
     public static void task1 ()    {
         System.out.println ("задача 1");
         int firstMan=35000;
-        byte secondMan=120;
+        byte secondMan=10;
         short  thirdMan=30000;
         long fourthMan= 220000;
-        float fifthMan= secondMan+1f;
-        double sixthMan=secondMan+2d;
-        System.out.println (firstMan);
-        System.out.println (secondMan);
-        System.out.println(thirdMan);
-        System.out.println(fourthMan);
-        System.out.println(fifthMan);
-        System.out.println (sixthMan);
+        float fifthMan= 5.58754f;
+        double sixthMan=5.9844464944674944012d;
+        System.out.println ("Значение переменной firstMan с типом int равно" +firstMan);
+        System.out.println ("Значение переменной secondMan с типом byte равно" +secondMan);
+        System.out.println("Значение переменной thirdMan с типом short равно" +thirdMan);
+        System.out.println("Значение переменной fourthMan с типом long равно" +fourthMan);
+        System.out.println("Значение переменной float с типом long равно" +fifthMan);
+        System.out.println ("Значение переменной sixthMan с типом double равно" +sixthMan);
     }
     public static void task2 ()    {
         System.out.println ("задача 2");
