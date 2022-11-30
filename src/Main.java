a122b10bccad7f94b3c2565da4dcc2e3dba9b006
Arikpele <79112835522@y.ru>
2022-11-30 11:17:59 +0300

    задание1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int firstMan=35000;
+        byte secondMan=120;
+        short  thirdMan=30000;
+        long fourthMan= 220000;
+        float fifthMan= secondMan+1f;
+        double sixthMan=secondMan+2d;
+        System.out.println (firstMan);
+        System.out.println (secondMan);
+        System.out.println(thirdMan);
+        System.out.println(fourthMan);
+        System.out.println(fifthMan);
+        System.out.println (sixthMan);
     }
 }
